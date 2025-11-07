c10cb85090b7c3ebe3c29d0490dd92dd83c36f42
gumeeee <gaxco123@gmail.com>
2025-11-06 23:46:25 -0300

    feat: add Font Awesome icons to header component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  faSearch = faSearch;
 }
