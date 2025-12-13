d61c6ea3e94a0feeeadc0a487dc6ddb4c2123fee
gumeeee <gaxco123@gmail.com>
2025-12-13 14:49:26 -0300

    feat: restructure sidenavigation component layout and add icon for dropdown

--- src/app/components/home/sidenavigation/sidenavigation.ts
+++ src/app/components/home/sidenavigation/sidenavigation.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidenavigation',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './sidenavigation.html',
   styleUrl: './sidenavigation.css',
 })
 export class Sidenavigation {
-
+  faAngleDown = faAngleDown;
 }
