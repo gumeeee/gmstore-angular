c21fd929546a3bdd86e32a6bd57d46876dac276f
gumeeee <gaxco123@gmail.com>
2025-12-14 22:54:31 -0300

    feat: add ratings component with template, styles, and tests; integrate ratings into product display

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ProductsService } from './products.service';
 import { ProductListItem } from './products.type';
 import { CommonModule } from '@angular/common';
+import { Ratings } from '../ratings/ratings';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule],
+  imports: [CommonModule, Ratings],
   templateUrl: './products.html',
   styleUrl: './products.css',
   providers: [ProductsService],
