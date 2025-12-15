874642223c67d520adabb79e9a7f1db5462610b1
gumeeee <gaxco123@gmail.com>
2025-12-14 21:39:35 -0300

    feat: add products component with template, styles, and tests; integrate sidenavigation in home component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { Catnavigation } from './catnavigation/catnavigation';
 import { Sidenavigation } from './sidenavigation/sidenavigation';
+import { Products } from '../products/products';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Catnavigation, Sidenavigation],
+  imports: [Header, Catnavigation, Sidenavigation, Products],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
